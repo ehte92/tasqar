eccb53bf89a62aef016df8b7c43243a81ac18004
Ehtesham <ehte92@gmail.com>
2024-10-09 20:07:52 +0300

    updated next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
-  // ... other configurations
 };
 
-module.exports = nextConfig;
+export default nextConfig;
