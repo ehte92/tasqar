9cfc09ed9b5ad4257fd0cc87abe56af9b6a88d0f
Ehtesham <ehte92@gmail.com>
2024-10-14 23:03:46 +0300

    Refactor Jest configuration and test utilities

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testEnvironment: 'jest-environment-jsdom',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  testMatch: ['**/__tests__/**/*.ts?(x)', '**/?(*.)+(spec|test).ts?(x)'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+};
+
+module.exports = createJestConfig(customJestConfig);
---
+++ src/types/test-utils.d.ts
@@ -0,0 +1,17 @@
+import { ReactElement } from 'react';
+import { RenderOptions, RenderResult } from '@testing-library/react';
+
+declare global {
+  namespace jest {
+    interface Matchers<R> {
+      toBeInTheDocument(): R;
+    }
+  }
+}
+
+declare module '@testing-library/react' {
+  function render(
+    ui: ReactElement,
+    options?: Omit<RenderOptions, 'queries'>
+  ): RenderResult;
+}
