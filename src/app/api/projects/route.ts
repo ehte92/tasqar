4beecfe49e62a5fd58bd07ef213064b9043175a3
Ehtesham <ehte92@gmail.com>
2024-10-15 21:56:39 +0300

    Refactor project route API file

--- src/app/api/projects/route.ts
+++ src/app/api/projects/route.ts
@@ -1,8 +1,9 @@
+import { getServerSession } from 'next-auth/next';
 import { NextResponse } from 'next/server';
 import { z } from 'zod';
-import prisma from '@/lib/db';
-import { getServerSession } from 'next-auth/next';
+
 import { authOptions } from '@/app/api/auth/[...nextauth]/route';
+import prisma from '@/lib/db';
 
 const projectSchema = z.object({
   title: z
